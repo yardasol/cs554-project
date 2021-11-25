2fb056d47a32b1df6e3beaa96bf96037701f69c2
yardasol <yardasol@grinnell.edu>
2021-11-24 18:10:49 -0600

    overhaul ilu function

--- Code/Helpers/preconditioner_solve.c
+++ Code/Helpers/preconditioner_solve.c
@@ -10,9 +10,38 @@ struct PCret {
     float* sol;
 };
 
+//conssuder using pointers for the setup and update helper functions
+int setupL (int nnz, struct A_csr *L_ptr){
+    struct L;
+    L_ptr->val = malloc(sizeof(float*) * (nnz));
+    L_ptr->col_ind = malloc(sizeof(int*) * (nnz));
+    L_ptr->row_ptr = malloc(sizeof(int*) * (nnz + 1));
+    L_ptr->val[0] = 1; L_ptr->col_ind[0] = 0; L_ptr->row_ptr[0] = 0;
+    return 0;
+}
+
+int updateL(){
+}
+
+int setupU(int nnz, int n_diag, struct A_csr *U_ptr, struct A_csr *A_ptr){
+    U_ptr->val = malloc(sizeof(float*) * (nnz));
+    U_ptr->col_ind = malloc(sizeof(int*) * (nnz));
+    U_ptr->row_ptr = malloc(sizeof(int*) * (nnz + 1));
+    for (int l = 0; l < n_diag; l++){
+        U_ptr->val[l] = A_ptr->val[l];
+	U_ptr->col_ind[l] = A_ptr->col_ind[l];
+	U_ptr->row_ptr[l] = A_ptr->row_ptr[l];
+    }
+    return 0;
+}
+
+int updateU(){
+}
+
 // Get ILU in-place factorization
 // hardcoded for 1D poisson matrix structure right now
-struct A_csr* getILU(int n, struct A_csr A){
+// WE MAY NEED TO USE POINTERS HERE to return the funcs
+int getILU(int n, struct A_csr A, struct A_csr arr[]){
     int i, nnz, n_diag;
     struct A_csr L, U;
     n_diag = 2; //hardcoded
@@ -21,58 +50,48 @@ struct A_csr* getILU(int n, struct A_csr A){
     n_diag_A = 3;
     nnz_A = n*n_diag_A - 2;
 
-    L.val = malloc(sizeof(float*) * (nnz));
-    L.col_ind = malloc(sizeof(int*) * (nnz));
-    L.row_ptr = malloc(sizeof(int*) * (nnz + 1));
-    L.val[0] = 1; L.col_ind[0] = 0; L.row_ptr[0] = 0;
-
-    U.val = malloc(sizeof(float*) * (nnz));
-    U.col_ind = malloc(sizeof(int*) * (nnz));
-    U.row_ptr = malloc(sizeof(int*) * (nnz + 1));
-    for (int l = 0; l < n_diag; l++){
-        U.val[l] = A.val[l];
-	U.col_ind[l] = A.col_ind[l];
-	U.row_ptr[l] = A.row_ptr[l];
-    }
+    setupL(nnz, &L);
+    setupU(nnz, n_diag, &U, &A);
 
     int cnt_L = 1;
     int cnt_U = n_diag; 
     int cnt_A = n_diag;
-    int i,j,k;
-    // ijk version
-    for (i = 1; i < n; i++){
-	for (k = 0; k < i; k++){
-      	    c = //current a index //Update lower diag of L
-	    d = (i+1)*n_diag_A - k//index of A diagonal
-	    L.val[cnt_L] = A.val[l]/A.val[d];
-	    L.col_ind[cnt_L] = A.col_ind[l];
-	    L.row_prt[cnt_L] = A.row_ptr[l];
-	    for (j = k+1; k < n; k++){
-	      // update U using L
-	    }
-	}
-	//add 1 to main diag of L
-	index_A = ...
-	L.val[cnt_L] = 1;
-	L.col_ind[cnt_L] = A.col_ind[index_A];
-	L.row_prt[cnt_L] = A.row_ptr[index_A];
-	cnt_L += 1;
-    }
 
-    //single index version
-    for (l = 1; l < nnz_A; l++){
+    int diag, row, col;
+    int index_L_ik, index_U_kj;
+    while (cnt_A < nnz_A){
+      row = A.row_ptr[cnt_A];
+      col = A.col_ind[cnt_A];
+      if (row > col){
 	//update L
-	if (A.col_ind[l] > A.row_ptr[l]){
-	  d = //index of diagonal
-	  L.val[cnt_L] = A.val[l]/A.val[d];
-	  L.col_ind[cnt_L] = A.col_ind[l];
-	  L.row_prt[cnt_L] = A.row_ptr[l];
+	diag = cnt_A;
+	while (A.row_ptr[diag] > A.col_ind[diag]) {diag += 1;}
+	L.val[cnt_L] = A.val[cnt_A]/A.val[diag];
+	L.col_ind[cnt_L] = col;
+	L.row_ptr[cnt_L] = row;
+	cnt_L += 1;
+      }
+      else {
+	index_L_ik = cnt_L-1;
+	//add 1 to main diag of L
+	if (row == col){
+	  L.val[cnt_L] = 1;
+	  L.col_ind[cnt_L] = col;
+	  L.row_ptr[cnt_L] = row;
+	  cnt_L += 1;
 	}
+	index_U_kj = ...;
 	//update U
-	else if (A.col_ind[l] <= A.row_ptr[l]){		
-	  U.val[cnt_U] = A.val[l]
-	}
+	U.val[cnt_U] = A.val[cnt_A] - L.val[index_L_ik]*U.val[index_U_kj];
+	U.row_ptr[cnt_U] = row;
+	U.col_ind[cnt_U] = col;
+	cnt_U += 1
+      }	      
+      cnt_A += 1;
     }
+    arr[0] = L;
+    arr[1] = U;
+    return 0;
 }
 
 /* Create an ILU-preconditioned A */
@@ -127,7 +146,7 @@ struct PCret Jacobi_PC_Solve(int n, struct A_csr A, float* r){
 
 struct PCret ILU_PC_Solve(int n, struct A_csr A, float* r){
     struct PCret pcret;
-    struct A_csr ILU = ...;
+    struct A_csr  = ...;
     float* z = create1dZeroVec(n);
     pcret.diag = getILU(n,A);
     for(int i = 0; i < n; i++){
