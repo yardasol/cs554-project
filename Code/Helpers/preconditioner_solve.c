95c5cec82968230fb9a9f928dd88be25feb92dbe
yardasol <yardasol@grinnell.edu>
2021-11-26 14:03:29 -0600

    added triangular solve to ILU_PC_Solve

--- Code/Helpers/preconditioner_solve.c
+++ Code/Helpers/preconditioner_solve.c
@@ -38,26 +38,51 @@ struct PCret Jacobi_PC_Solve(int n, struct A_csr A, float* r){
     return pcret;
 }
 
-struct PCret ILU_PC_Solve(int n, struct A_csr A, float* r){
+struct PCret ILU_PC_Solve(int n, int n_diag, int offset, struct A_csr A, float* r){
     struct PCret pcret;
-    struct A_csr L, U; 
-    float* z = create1dZeroVec(n);
-    pcret.diag = getILU(n,A);
-    for(int i = 0; i < n; i++){
-        z[i] = r[i]/pcret.diag[i];
-    }
+    struct A_csr ILU, L, U;
+	ILU = createPoissonIncompleteLUCSR(n, n_diag, offset, &A);
+	L = getLfromPoissonILUCSR(n, n_diag, offset, &ILU);
+	U = getUfromPoissonILUCSR(n, n_diag, offset, &ILU);
+
+	int l = 0; //CSR index
+
+	// Ly = r
+	float *y = create1dZeroVec(n);
+	for (int i = 0; i < n; i++){
+		while (L.row_ptr[l+1] == i){
+			y[i] -= L.val[l] * y[L.col_ind[l]];
+			l++;
+		}
+		y[i] += r[i];
+		y[i] = y[i] / L.val[l];
+	}
+
+	// Uz = y;
+	float *z = create1dZeroVec(n);
+	for (int i = n-1; i >= 0  ; i--){
+		while (L.row_ptr[l-1] == i){
+			z[i] -= L.val[l] * z[L.col_ind[l]];
+			l--;
+		}
+		z[i] += y[i];
+		z[i] = z[i] / L.val[l];
+	}
     pcret.sol = z;
     return pcret;
 }
 
 /*Preconditioner solve*/
-struct PCret PC_Solve(int n, float* r, struct A_csr A, char* pctype){
+struct PCret PC_Solve(int n, int n_diag, int offset, float* r, struct A_csr A, char* pctype){
 
     struct PCret pcret;
 
     if (strcmp(pctype,"Jacobi")==0){
     	pcret = Jacobi_PC_Solve(n,A,r);
     }
+	if (stcmp(pctype, "ILU")==0){
+		pcret = ILU_PC_Solve(n, n_diag, offset, A, r);
+	}
 
     return pcret;
 }
