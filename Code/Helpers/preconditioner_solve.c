476a2ace5f6701ffa270ff7157d297c20871d105
yardasol <yardasol@grinnell.edu>
2021-11-26 14:29:27 -0600

    changed variables of type A_csr to pointers to variables of that type

--- Code/Helpers/preconditioner_solve.c
+++ Code/Helpers/preconditioner_solve.c
@@ -11,14 +11,14 @@ struct PCret {
 };
 
 // Extract diagonal from CSR matrix
-float* getCSRdiagonal(int n, struct A_csr A){
+float* getCSRdiagonal(int n, struct A_csr *A_ptr){
     float* d = create1dZeroVec(n);
     int col=0;
     for (int i = 0; i<n; i++){   
-        for (int j=A.row_ptr[i]; j<A.row_ptr[i+1]; j++){
-            col = A.col_ind[j];
+        for (int j=A_ptr->row_ptr[i]; j<A_ptr->row_ptr[i+1]; j++){
+            col = A_ptr->col_ind[j];
             if(col==i){
-                d[col] = A.val[j];
+                d[col] = A_ptr->val[j];
                 col++; 
                 if(col>n){break;}   
                 }
@@ -27,10 +27,10 @@ float* getCSRdiagonal(int n, struct A_csr A){
     return d; 
 }
 
-struct PCret Jacobi_PC_Solve(int n, struct A_csr A, float* r){
+struct PCret Jacobi_PC_Solve(int n, struct A_csr *A_ptr, float* r){
     struct PCret pcret;
     float* z = create1dZeroVec(n);
-    pcret.diag = getCSRdiagonal(n,A);
+    pcret.diag = getCSRdiagonal(n,A_ptr);
     for(int i = 0; i < n; i++){
         z[i] = r[i]/pcret.diag[i];
     }
@@ -38,50 +38,45 @@ struct PCret Jacobi_PC_Solve(int n, struct A_csr A, float* r){
     return pcret;
 }
 
-struct PCret ILU_PC_Solve(int n, int n_diag, int offset, struct A_csr A, float* r){
+struct PCret ILU_PC_Solve(int n, struct A_csr *L_ptr, struct A_csr *U_ptr, float* r){
     struct PCret pcret;
-    struct A_csr ILU, L, U;
-	ILU = createPoissonIncompleteLUCSR(n, n_diag, offset, &A);
-	L = getLfromPoissonILUCSR(n, n_diag, offset, &ILU);
-	U = getUfromPoissonILUCSR(n, n_diag, offset, &ILU);
-
 	int l = 0; //CSR index
 
 	// Ly = r
 	float *y = create1dZeroVec(n);
 	for (int i = 0; i < n; i++){
-		while (L.row_ptr[l+1] == i){
-			y[i] -= L.val[l] * y[L.col_ind[l]];
+		while (L_ptr->row_ptr[l+1] == i){
+			y[i] -= L_ptr->val[l] * y[L_ptr->col_ind[l]];
 			l++;
 		}
 		y[i] += r[i];
-		y[i] = y[i] / L.val[l];
+		y[i] = y[i] / L_ptr->val[l];
 	}
 
 	// Uz = y;
 	float *z = create1dZeroVec(n);
 	for (int i = n-1; i >= 0  ; i--){
-		while (L.row_ptr[l-1] == i){
-			z[i] -= L.val[l] * z[L.col_ind[l]];
+		while (U_ptr->row_ptr[l-1] == i){
+			z[i] -= U_ptr->val[l] * z[U_ptr->col_ind[l]];
 			l--;
 		}
 		z[i] += y[i];
-		z[i] = z[i] / L.val[l];
+		z[i] = z[i] / U_ptr->val[l];
 	}
     pcret.sol = z;
     return pcret;
 }
 
 /*Preconditioner solve*/
-struct PCret PC_Solve(int n, int n_diag, int offset, float* r, struct A_csr A, char* pctype){
+struct PCret PC_Solve(int n, float* r, struct A_csr *A_ptr, struct A_csr *L_ptr, struct A_csr *U_ptr, char* pctype){
 
     struct PCret pcret;
 
     if (strcmp(pctype,"Jacobi")==0){
-    	pcret = Jacobi_PC_Solve(n,A,r);
+    	pcret = Jacobi_PC_Solve(n,A_ptr,r);
     }
 	if (stcmp(pctype, "ILU")==0){
-		pcret = ILU_PC_Solve(n, n_diag, offset, A, r);
+		pcret = ILU_PC_Solve(n, L_ptr, U_ptr, r);
 	}
 
     return pcret;
